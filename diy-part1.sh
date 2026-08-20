e3ef8ad0cd37fbb47c77cbaa1eb8d1870abf2714
wrg1125 <bhmzhy@outlook.com>
2026-08-20 20:34:21 +0800

    配置官方OpenWrt固件: x86/64, 1G rootfs, LAN 10.0.0.1, PPPoE, LuCI中文, root密码

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# OpenWrt DIY script part 1 (Before Update feeds)
+# 官方源码 (openwrt/openwrt): 不添加第三方 feed，保持纯净
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
